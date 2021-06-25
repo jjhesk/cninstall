3db7b6fd39957649bf59d8e2ccdd674a3ee9c6de
heskdx <jobhesk@gmail.com>
2021-06-25 19:09:52 +0800

    compile success modification v1.4.85

--- bt_patch.sh
+++ bt_patch.sh
@@ -5,6 +5,7 @@ echo; echo 'Installing BT patch 1.01'; echo
 RELEASE_TAG=0.990
 FILE1=https://cdn.jsdelivr.net/gh/ONode/BTB@$RELEASE_TAG/BTPanel/static/js/index.js
 FILE2=https://cdn.jsdelivr.net/gh/ONode/BTB@$RELEASE_TAG/BTPanel/__init__.py
+FILE3=https://cdn.jsdelivr.net/gh/ONode/BTB@$RELEASE_TAG/BTPanel/templates/default/bind.html
 BASE_PANEL=/www/server/panel
 
 echo 'search for version'
@@ -18,6 +19,7 @@ fi
 
 wget -q $FILE1 -O $BASE_PANEL/BTPanel/static/js/index.js
 wget -q $FILE2 -O $BASE_PANEL/BTPanel/__init__.py
+wget -q $FILE3 -O $BASE_PANEL/BTPanel/templates/default/bind.html
 
 cd $BASE_PANEL
 
